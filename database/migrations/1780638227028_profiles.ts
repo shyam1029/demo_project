7437d2af4ac8f6888b805632096b16e720c8a76b
syamsatishpolipalli <syamsatishpolipalli@gmail.com>
2026-06-05 12:18:11 +0530

    feat: add profiles migration

---
+++ database/migrations/1780638227028_profiles.ts
@@ -0,0 +1,26 @@
+import BaseSchema from "@ioc:Adonis/Lucid/Schema";
+export default class Profiles extends BaseSchema {
+  protected tableName = "profiles";
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments("id");
+      table
+        .integer("user_id")
+        .unsigned()
+        .references("id")
+        .inTable("users")
+        .onDelete("CASCADE")
+        .notNullable()
+        .unique();
+      table.string("name", 30).notNullable();
+      table.string("mobile", 10).notNullable();
+      table.enum("gender", ["MALE", "FEMALE"]).notNullable();
+      table.date("date_of_birth").notNullable();
+      table.timestamp("created_at", { useTz: true }).notNullable();
+      table.timestamp("updated_at", { useTz: true }).notNullable();
+    });
+  }
+  public async down() {
+    this.schema.dropTable(this.tableName);
+  }
+}
