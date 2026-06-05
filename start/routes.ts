36a9a79cf88b7df0bbaf1c773b9492c85e226a48
syamsatishpolipalli <syamsatishpolipalli@gmail.com>
2026-06-05 15:12:45 +0530

    feat: routes

--- start/routes.ts
+++ start/routes.ts
@@ -18,27 +18,22 @@
 |
 */
 
-import Route from '@ioc:Adonis/Core/Route'
+import Route from "@ioc:Adonis/Core/Route";
 
-Route.get('/', async () => {
-  return { hello: 'world' }
-})
+Route.get("/", async () => {
+  return { hello: "world" };
+});
 
 Route.group(() => {
-
   Route.post("register", "AuthController.register");
   Route.post("login", "AuthController.login");
-  
-      Route.group( () => {
-      Route.get("profiles/:id", "ProfilesController.show");
-      Route.put("profiles/update", "ProfilesController.update");
-      Route.post("profiles", "ProfilesController.store");
-      }).middleware("auth:api");
 
-      Route.group(() => {
-        Route.post("profiles/delete", "ProfilesController.destroy"),
-        Route.get("profiles", "ProfilesController.show")
-      }).middleware(["auth", "admin"])
-      
-      
+  Route.group(() => {
+    Route.get("/profile", "ProfilesController.show");
+    Route.post("/profile", "ProfilesController.create");
+    Route.put("/profile", "ProfilesController.update");
+    Route.delete("/profile", "ProfilesController.destroy");
+  })
+    .prefix("/user")
+    .middleware("auth");
 }).prefix("api");
