4e53befe2e2547edeab809cbe61ea5caa476b306
syamsatishpolipalli <syamsatishpolipalli@gmail.com>
2026-06-05 12:17:45 +0530

    refactor: update user migration and remove others

--- database/migrations/1698303260183_users.ts
+++ database/migrations/1698303260183_users.ts
@@ -1,26 +1,20 @@
-import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+import BaseSchema from "@ioc:Adonis/Lucid/Schema";
 
 export default class extends BaseSchema {
-  protected tableName = 'users'
+  protected tableName = "users";
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').primary()
-      table.integer('role_id')
-      table.string('username', 255).notNullable()
-      table.string('email', 255).notNullable().unique()
-      table.string('password', 180).notNullable()
-      table.string('remember_me_token').nullable()
-
-      /**
-       * Uses timestampz for PostgreSQL and DATETIME2 for MSSQL
-       */
-      table.timestamp('created_at', { useTz: true }).notNullable()
-      table.timestamp('updated_at', { useTz: true }).notNullable()
-    })
+      table.increments("id").primary();
+      table.string("email", 255).notNullable().unique();
+      table.string("password", 180).notNullable();
+      table.string("remember_me_token").nullable();
+      table.timestamp("created_at", { useTz: true }).notNullable();
+      table.timestamp("updated_at", { useTz: true }).notNullable();
+    });
   }
 
   public async down() {
-    this.schema.dropTable(this.tableName)
+    this.schema.dropTable(this.tableName);
   }
 }
